b190b9c9f2b5b3abbd54a7a2ef4ac3c1135558d2
GonzaloMainardi <gonzalomainardi21@gmail.com>
2023-10-06 00:47:31 -0300

    primeras lineas de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import NavBar from "./components/navbar/navbar";
+import ItemListContainer from "./components/ItemListContainer/itemlistcontainer";
+
 
 function App() {
+  const mensaje = "¡Bienvenidx a HI FIVE, la tecnologia al alcance de tu mano!";
 
   return (
     <>
       <NavBar />
+      <ItemListContainer mensaje = {mensaje} />
     </>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,77 +1,25 @@
-import React, { useState } from 'react';
-import {
-  HomeOutlined,
-  TeamOutlined,
-  ShoppingCartOutlined,
-  WhatsAppOutlined,
-  BarsOutlined
-} from '@ant-design/icons';
-import {Layout, Menu, theme } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const items = [
-  getItem('INICIO', '1', <HomeOutlined />),
-  getItem('NOSOTROS', '2', <TeamOutlined />),
-  getItem('PRODUCTOS', '3', <BarsOutlined />),
-  getItem('CARRITO', '4', <ShoppingCartOutlined />),
-  getItem('CONTACTO', '5', <WhatsAppOutlined />),
-];
-const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-  return (
-    <Layout
-      style={{
-        minHeight: '100vh',
-      }}
-    >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-            minWidth: '85vw',
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styles from './styles.module.css';
 
-          }}
-        >
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              
-            }}
-          >
-          </div>
-        </Content>
-        <Footer
-          style={{
-            minWidth: '85vw',
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©2023 Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
+const NavBar = () =>   {
+  return (
+    <Navbar expand="lg" className={styles.Navbar}>
+      <Container className={styles.container}>
+        <Navbar.Brand className={styles.menuNav} href="#home">HI FIVE</Navbar.Brand>
+            <Navbar.Collapse>
+                <Nav className={styles.nav}>
+                    <Nav.Link className={styles.menuNav} href="#home">INICIO</Nav.Link>
+                    <Nav.Link className={styles.menuNav} href="#link">PRODUCTOS</Nav.Link>
+                    <Nav.Link className={styles.menuNav} href="#home">CARRITO</Nav.Link>
+                    <Nav.Link className={styles.menuNav} href="#link">NOSOTROS</Nav.Link>
+                    <Nav.Link className={styles.menuNav} href="#home">CONTACTO</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
-export default App;
+}
+
+export default NavBar;
