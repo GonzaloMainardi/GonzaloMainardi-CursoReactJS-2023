d0a0d99be8c4eb30299b2cfc67b2e071875a905a
GonzaloMainardi <gonzalomainardi21@gmail.com>
2023-10-06 01:42:50 -0300

    PRIMERA PREENTREGA

--- src/components/CardWidget/cardwidget.jsx
+++ src/components/CardWidget/cardwidget.jsx
@@ -1,10 +1,13 @@
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import {ShoppingCartOutlined } from '@ant-design/icons';
+
 
 const CardWidget = () => {
   return (
-    <Button variant="primary">
-      CARRITO <Badge bg="success">10</Badge>
+    <Button variant="success">
+       <ShoppingCartOutlined />
+       <Badge bg="success"> 5</Badge>
     </Button>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from './navbar.module.css';
-import CardWidget from '../CartWidget/cartwidget';
+import CardWidget from '../CardWidget/cardwidget';
 
 
 
